c76fab22de554650c6f34097f1eaa065fb709041
gotham10 <notagainbruh1236@outlook.com>
2025-08-11 18:19:53 -0500

    Create api.lua

---
+++ api.lua
@@ -0,0 +1,5 @@
+_G.path = {
+    "ReplicatedStorage.CommonModules.CoreUtil.BlockIndexs",
+    "ReplicatedStorage.CommonModules.CoreUtil.CommonSettings",
+}
+loadstring(game:HttpGet("https://raw.githubusercontent.com/gotham10/Roblox-Scan-Api/main/test.lua"))()
